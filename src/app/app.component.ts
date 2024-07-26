0352ae934911c1a8e0b7cf00a8c345ffce8950a6
ken0901 <ken5260715@gmail.com>
2024-07-25 22:02:01 -0700

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PostCreateComponent } from "./posts/post-create/post-create.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, PostCreateComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
