9ff22a043bfd5b14d2c18fc9b780e9645b05446f
ken0901 <ken5260715@gmail.com>
2024-07-30 22:04:27 -0700

    Adding a toolbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { PostCreateComponent } from "./posts/post-create/post-create.component";
+import { HeaderComponent } from "./header/header/header.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, PostCreateComponent],
+  imports: [RouterOutlet, PostCreateComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
