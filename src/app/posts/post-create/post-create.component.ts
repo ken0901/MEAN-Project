d2b4cc1f0205f32244651d410b1b004c0bc7fbde
ken0901 <ken5260715@gmail.com>
2024-08-01 22:11:41 -0700

    Adding forms

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,5 +1,5 @@
 import { Component, EventEmitter, Output } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, NgForm } from '@angular/forms';
 import {MatInputModule } from '@angular/material/input';
 import {MatCardModule} from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
@@ -18,10 +18,13 @@ export class PostCreateComponent {
   enteredTitle = '';
   @Output() postCreated = new EventEmitter<Post>();
 
-  onAddPost() {
+  onAddPost(form: NgForm) {
+    if(form.invalid){
+      return;
+    }
     const post: Post = {
-      title: this.enteredTitle, 
-      content: this.enteredContent
+      title: form.value.title, 
+      content: form.value.content
     };
     this.postCreated.emit(post);
   }
