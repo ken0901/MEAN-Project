891cfd2ed6c868bccec016d4cd5a7448e333ce2c
ken0901 <ken5260715@gmail.com>
2024-08-02 21:23:11 -0700

    Calling GET Post

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,10 +1,10 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component } from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
 import {MatInputModule } from '@angular/material/input';
 import {MatCardModule} from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { CommonModule } from '@angular/common';
-import { Post } from '../post.model';
+import { PostsService } from '../posts.service';
 
 @Component({
   selector: 'app-post-create',
@@ -14,19 +14,17 @@ import { Post } from '../post.model';
   styleUrl: './post-create.component.css'
 })
 export class PostCreateComponent {
-  enteredContent = '';
-  enteredTitle = '';
-  @Output() postCreated = new EventEmitter<Post>();
+  enteredContent = "";
+  enteredTitle = "";
+
+  constructor(private postService: PostsService) {}
 
   onAddPost(form: NgForm) {
     if(form.invalid){
       return;
     }
-    const post: Post = {
-      title: form.value.title, 
-      content: form.value.content
-    };
-    this.postCreated.emit(post);
+    
+    this.postService.addPost(form.value.title,  form.value.content);
   }
   // onAddPost(postInput: HTMLTextAreaElement) {
   //   this.newPost = postInput.value;
--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -1,8 +1,9 @@
 import { CommonModule } from '@angular/common';
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { Post } from '../post.model';
 import { PostsService } from '../posts.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-post-list',
@@ -11,13 +12,25 @@ import { PostsService } from '../posts.service';
   templateUrl: './post-list.component.html',
   styleUrl: './post-list.component.css'
 })
-export class PostListComponent {
+export class PostListComponent implements OnInit, OnDestroy{
   // posts = [
   //   {title: "First Post", content: "This is the first post\'s content"},
   //   {title: "Second Post", content: "This is the second post\'s content"},
   //   {title: "Third Post", content: "This is the third post\'s content"},
   // ];
-  @Input() posts: Post[] = [];
+  posts: Post[] = [];
+  private postsSub: Subscription;
 
   constructor(private postsService: PostsService) {}
+
+  ngOnInit(): void {
+    this.posts = this.postsService.getPost();
+    this.postsSub = this.postsService.getPostUpdateListener().subscribe((posts: Post[]) => {
+      this.posts = posts;
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.postsSub.unsubscribe();
+  }
 }
