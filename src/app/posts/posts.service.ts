aa4a31dde060fedc2c1bcb4e5789da06f43b46f5
ken0901 <ken5260715@gmail.com>
2024-08-08 21:51:26 -0700

    Transforming Response Data

--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from "@angular/core";
 import { Post } from "./post.model";
-import { Subject } from "rxjs";
+import { map, Subject } from "rxjs";
 import { HttpClient } from "@angular/common/http";
 
 @Injectable({
@@ -13,10 +13,21 @@ export class PostsService {
     constructor(private http: HttpClient) {}
 
     getPost() {
-        this.http.get<{message: string, posts: Post[]}>('http://localhost:3000/api/posts').subscribe((postData) => {
-            this.posts = postData.posts;
-            this.postsUpdated.next([...this.posts]);
-        });
+        this.http.get<{message: string, posts: any}>('http://localhost:3000/api/posts')
+            .pipe(map((postData) => {
+                return postData.posts.map(post => {
+                    return {
+                        title: post.title,
+                        content: post.content,
+                        id: post._id
+                    };
+                });
+            }))
+            .subscribe((transformedPosts) => {
+                this.posts = transformedPosts;
+                this.postsUpdated.next([...this.posts]);
+            }
+        );
     }
 
     getPostUpdateListener() {
