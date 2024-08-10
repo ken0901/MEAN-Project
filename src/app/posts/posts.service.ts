d5ecb3362e66da66ba8aea8f09f71a1d508a009f
ken0901 <ken5260715@gmail.com>
2024-08-09 21:38:13 -0700

    Deleting Documents

--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -13,7 +13,7 @@ export class PostsService {
     constructor(private http: HttpClient) {}
 
     getPost() {
-        this.http.get<{message: string, posts: any}>('http://localhost:3000/api/posts')
+        this.http.get<{message: string, posts: any}>("http://localhost:3000/api/posts")
             .pipe(map((postData) => {
                 return postData.posts.map(post => {
                     return {
@@ -36,10 +36,16 @@ export class PostsService {
 
     addPost(title: string, content: string) {
         const post: Post = {id: null, title: title, content: content};
-        this.http.post<{message: string}>('http://localhost:3000/api/posts', post).subscribe((responseData) => {
+        this.http.post<{message: string}>("http://localhost:3000/api/posts", post).subscribe((responseData) => {
             console.log(responseData.message);
         });
         this.posts.push(post);
         this.postsUpdated.next([...this.posts]);
+    }
+
+    deletePost(postId: string) {
+        this.http.delete("http://localhost:3000/api/posts/" + postId).subscribe(() => {
+            console.log('Deleted!');
+        });
     }
 }
