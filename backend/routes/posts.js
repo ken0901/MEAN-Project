30965c0f32c959e736f00493aaa7c0a7b1c220cd
ken0901 <ken5260715@gmail.com>
2024-08-20 22:03:17 -0700

    Uploading files

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const multer = require("multer");
 const Post = require('../models/post');
+const router = express.Router();
 
 const MIME_TYPE_MAP = {
-    'image/png': 'png',
-    'image/jpeg': 'jpg',
-    'image/jpg': 'jpg'
-}
-const router = express.Router();
+    "image/png": "png",
+    "image/jpeg": "jpg",
+    "image/jpg": "jpg"
+};
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
         const isValid = MIME_TYPE_MAP[file.mimetype];
@@ -18,13 +18,13 @@ const storage = multer.diskStorage({
         cb(error, "backend/images");
     },
     filename: (req, file, cb) => {
-        const name = file.originalname.toLowerCase().split(' ').join('-');
+        const name = file.originalname.toLowerCase().split(" ").join("-");
         const ext = MIME_TYPE_MAP[file.mimetype];
-        cb(null, name + '-' + Date.now() + '.' + ext);
+        cb(null, name + "-" + Date.now() + "." + ext);
     }
 });
 
-router.post("", (req, res, next) => {
+router.post("", multer({storage: storage}).single("image") ,(req, res, next) => {
     const post = new Post({
         title: req.body.title,
         content: req.body.content
@@ -37,7 +37,7 @@ router.post("", (req, res, next) => {
     });
 });
 
-router.get("", multer(storage).single("image") ,(req, res, next) => {
+router.get("" ,(req, res, next) => {
     Post.find().then(documents => {
         res.status(200).json({
             message: "Posts fetched successfully",
--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -41,11 +41,17 @@ export class PostsService {
         return this.http.get<{_id: string, title: string, content: string}>("http://localhost:3000/api/posts/" + id);
     }
 
-    addPost(title: string, content: string) {
-        const post: Post = {id: null, title: title, content: content};
-        this.http.post<{message: string, postId: string}>("http://localhost:3000/api/posts", post).subscribe((responseData) => {
-            const id = responseData.postId;
-            post.id = id;
+    addPost(title: string, content: string, image: File) {
+        //const post: Post = {id: null, title: title, content: content};
+        const postData = new FormData();
+        postData.append("title", title);
+        postData.append("content", content);
+        postData.append("image", image, title);
+        // postData.forEach((value, key) => {   
+        //     console.log(key, value) 
+        // });
+        this.http.post<{message: string, postId: string}>("http://localhost:3000/api/posts", postData).subscribe((responseData) => {
+            const post: Post = {id: responseData.postId, title: title, content: content};
             this.posts.push(post);
             this.postsUpdated.next([...this.posts]);
             this.router.navigate(["/"]);
