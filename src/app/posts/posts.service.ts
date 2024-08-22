e1f7457eec2422928b7b9dadc0307be4d2ce5a51
ken0901 <ken5260715@gmail.com>
2024-08-21 21:44:16 -0700

    Working with file URL

--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -21,7 +21,8 @@ export class PostsService {
                     return {
                         title: post.title,
                         content: post.content,
-                        id: post._id
+                        id: post._id,
+                        imagePath: post.imagePath
                     };
                 });
             }))
@@ -50,8 +51,13 @@ export class PostsService {
         // postData.forEach((value, key) => {   
         //     console.log(key, value) 
         // });
-        this.http.post<{message: string, postId: string}>("http://localhost:3000/api/posts", postData).subscribe((responseData) => {
-            const post: Post = {id: responseData.postId, title: title, content: content};
+        this.http.post<{message: string, post: Post}>("http://localhost:3000/api/posts", postData).subscribe((responseData) => {
+            const post: Post = {
+                id: responseData.post.id, 
+                title: title, 
+                content: content,
+                imagePath: responseData.post.imagePath
+            };
             this.posts.push(post);
             this.postsUpdated.next([...this.posts]);
             this.router.navigate(["/"]);
@@ -67,7 +73,7 @@ export class PostsService {
     }
 
     updatePost(id: string, title: string, content: string) {
-        const post: Post = { id: id, title: title, content: content};
+        const post: Post = { id: id, title: title, content: content, imagePath: null};
         this.http.put("http://localhost:3000/api/posts/" + id, post)
             .subscribe(response => {
                 const updatedPosts = [...this.posts];
