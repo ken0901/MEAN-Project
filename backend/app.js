6f576ee6803435612c6953c77bede2a9672d0a40
ken0901 <ken5260715@gmail.com>
2024-08-22 21:23:21 -0700

    Fetching images on the frontend

--- backend/app.js
+++ backend/app.js
@@ -1,3 +1,4 @@
+const path = require("path");
 const express =require('express');
 const bodyParser = require("body-parser");
 const mongoose = require('mongoose');
@@ -18,6 +19,7 @@ mongoose.connect("mongodb://127.0.0.1:27017/mean-project")
 
 app.use((bodyParser.json()));
 app.use(bodyParser.urlencoded({extended: false}));
+app.use("/images", express.static(path.join("backend/images")));
 
 app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
