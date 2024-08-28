e6d5a5f796375b940d2fa6c9dfc389e6e3ad9095
ken0901 <ken5260715@gmail.com>
2024-08-27 21:35:11 -0700

    Heandling User Input

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,20 +1,23 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { MatButton } from '@angular/material/button';
-import { MatCard } from '@angular/material/card';
+import { FormsModule, NgForm } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 import { MatError } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
-import { MatProgressSpinner } from '@angular/material/progress-spinner';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [FormsModule, CommonModule, MatButton,  MatCard, MatProgressSpinner, MatInputModule, MatError],
+  imports: [FormsModule, CommonModule, MatButtonModule,  MatCardModule, MatProgressSpinnerModule, MatInputModule, MatError],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
   isLoading = false;
 
+  onLogin(form: NgForm) {
+    console.log(form.value);
+  }
 }
