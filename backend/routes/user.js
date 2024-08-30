9797cafddf1390f62b1c278852396c617110951f
ken0901 <ken5260715@gmail.com>
2024-08-29 22:10:10 -0700

    SPA Authentication

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 const router = express.Router();
 const User = require("../models/user");
 
@@ -22,6 +23,31 @@ router.post("/signup", (req, res, next) => {
                         error: err
                     });
                 });
+        });
+});
+
+router.post("/login", (req, res, next) => {
+    User.find({email: req.body.email})
+        .then(user => {
+            if(!user) {
+                return res.status(401).json({
+                    message: "Auth failed"
+                });
+            }
+            return bcrypt.compare(req.body.password, user.password)
+        })
+        .then(result => {
+            if(!result) {
+                return res.status(401).json({
+                    message: "Auth failed"
+                });
+            }
+            const token = jwt.sign({email: user.email, userId: user._id}, 'secret_this_should_be_longer', {expiresIn: "1h" });
+        })
+        .catch(err => {
+            return res.status(401).json({
+                message: "Auth failed"
+            });
         });
 });
 
