c9874c4408fa4898fce87286c477ec5fa0172efc
ken0901 <ken5260715@gmail.com>
2024-09-03 21:51:04 -0700

    Adding the token to authenticate requests

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,12 +3,14 @@ import { provideRouter } from '@angular/router';
 import {provideAnimations} from '@angular/platform-browser/animations';
 
 import { routes } from './app.routes';
-import { provideHttpClient } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, provideHttpClient } from '@angular/common/http';
+import { AuthInterceptor } from './auth/auth-interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes), 
     provideAnimations(),
-    provideHttpClient()
+    provideHttpClient(),
+    {provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true}
   ]
 };
