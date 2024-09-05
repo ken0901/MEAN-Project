d1ec71653b66c64809ae9e1ab2e2dd983f1aefb6
ken0901 <ken5260715@gmail.com>
2024-09-04 21:19:44 -0700

    Improving the UI header to reflecct the authentication status

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { MatButton } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { AuthService } from '../auth/auth.service';
+import { Subscription } from 'rxjs';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [MatToolbarModule, RouterLink, MatButton, RouterLinkActive],
+  imports: [MatToolbarModule, RouterLink, MatButton, RouterLinkActive, CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit, OnDestroy{
+  private authListenerSubs: Subscription;
+  userIsAuthenticated = false;
 
+  constructor(private authService: AuthService) {}
+
+  ngOnInit(): void {
+    this.authListenerSubs = this.authService.getAuthStatusListener().subscribe(isauthenticated => {
+      this.userIsAuthenticated = isauthenticated;
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.authListenerSubs.unsubscribe();
+  }
 }
