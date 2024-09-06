4a437c7f3abd7f449a032c3ff2d68bb58538c3b1
ken0901 <ken5260715@gmail.com>
2024-09-05 21:29:23 -0700

    Adding Route Guards

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,12 +5,14 @@ import {provideAnimations} from '@angular/platform-browser/animations';
 import { routes } from './app.routes';
 import { HTTP_INTERCEPTORS, provideHttpClient } from '@angular/common/http';
 import { AuthInterceptor } from './auth/auth-interceptor';
+import { AuthGuard } from './auth/auth.guard';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes), 
     provideAnimations(),
     provideHttpClient(),
-    {provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true}
+    {provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true},
+    AuthGuard
   ]
 };
