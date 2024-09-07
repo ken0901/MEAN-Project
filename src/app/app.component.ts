8c39e4a315a70e81a19a645e472949894488f91b
ken0901 <ken5260715@gmail.com>
2024-09-06 22:17:12 -0700

    Saving the token in the local storage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { PostCreateComponent } from "./posts/post-create/post-create.component";
 import { HeaderComponent } from "./header/header.component";
 import { PostListComponent } from "./posts/post-list/post-list.component";
 import { Post } from './posts/post.model';
+import { AuthService } from './auth/auth.service';
 
 @Component({
   selector: 'app-root',
@@ -12,10 +13,16 @@ import { Post } from './posts/post.model';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   storedPosts: Post[] = [];
+
+  constructor(private authSerivce: AuthService) {}
 
   onPostAdded(post) {
     this.storedPosts.push(post);
+  }
+
+  ngOnInit(): void {
+    this.authSerivce.authAuthUser();  
   }
 }
