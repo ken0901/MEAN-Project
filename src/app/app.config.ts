7fca0a57edc6697f798ede76d109911a058fdd7d
ken0901 <ken5260715@gmail.com>
2024-09-09 21:55:23 -0700

    Adding a reference to the model and fixed auth intercepotr

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,7 @@ import { provideRouter } from '@angular/router';
 import {provideAnimations} from '@angular/platform-browser/animations';
 
 import { routes } from './app.routes';
-import { HTTP_INTERCEPTORS, provideHttpClient } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { AuthInterceptor } from './auth/auth-interceptor';
 import { AuthGuard } from './auth/auth.guard';
 
@@ -11,7 +11,7 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes), 
     provideAnimations(),
-    provideHttpClient(),
+    provideHttpClient(withInterceptorsFromDi()),
     {provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true},
     AuthGuard
   ]
--- src/app/auth/auth-interceptor.ts
+++ src/app/auth/auth-interceptor.ts
@@ -7,10 +7,10 @@ import { AuthService } from "./auth.service";
 export class AuthInterceptor implements HttpInterceptor {
     constructor(private authService: AuthService) {}
 
-    intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    intercept(req: HttpRequest<any>, next: HttpHandler) {
         const authToken = this.authService.getToken();
         const authRequest = req.clone({
-            headers: req.headers.set('Authorization',"Bearer " + authToken)
+            headers: req.headers.set("Authorization", "Bearer " + authToken)
         });
         return next.handle(authRequest);            
     }
