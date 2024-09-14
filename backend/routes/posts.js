8a8577ad179ca4f827a57c7e976e8196b8dad1cf
ken0901 <ken5260715@gmail.com>
2024-09-13 21:57:30 -0700

    Returning error messages on the server

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -41,6 +41,11 @@ router.post("", checkAuth, multer({storage: storage}).single("image") ,(req, res
                 id: createdPost._id,
             }
         });
+    })
+    .catch(error => {
+        res.status(500).json({
+            message: "Creating a post failed!"
+        });
     });
 });
 
@@ -62,6 +67,11 @@ router.get("" ,(req, res, next) => {
             posts: fetchedPosts,
             maxPosts: count
         });    
+    })
+    .catch(error => {
+        res.status(500).json({
+            message: "Fetching posts failed!"
+        });
     });
 });
 
@@ -72,6 +82,11 @@ router.delete("/:id", checkAuth, (req,res,next) => {
         } else {
             res.status(401).json({message: "Not Authorized!"});
         }
+    })
+    .catch(error => {
+        res.status(500).json({
+            message: "Fetching posts failed!"
+        });
     });
 });
 
@@ -94,6 +109,11 @@ router.put("/:id", checkAuth, multer({storage: storage}).single("image"), (req,r
         } else {
             res.status(401).json({message: "Not Authorized!"});
         }
+    })
+    .catch(error => {
+        res.status(500).json({
+            message: "Couldn't update post!"
+        });
     });
 });
 
@@ -104,6 +124,11 @@ router.get("/:id", (req,res,next) => {
         } else {
             res.status(404).json({message: "Post not found!"});
         }
+    })
+    .catch(error => {
+        res.status(500).json({
+            message: "Fetching post failed!"
+        });
     });
 });
 
--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -60,6 +60,8 @@ export class PostListComponent implements OnInit, OnDestroy{
   onDelete(postId: string) {
     this.postsService.deletePost(postId).subscribe(() => {
       this.postsService.getPosts(this.postsPerPage, this.currentPage);
+    }, () => {
+      this.isLoading = false;
     });
   }
 
