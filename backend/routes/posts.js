b400ce5cc6897af4444d0ed27cf08fe284bcc0ca
ken0901 <ken5260715@gmail.com>
2024-09-16 21:17:28 -0700

    Separting the middleware

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -1,38 +1,20 @@
 const express = require("express");
-const multer = require("multer");
+
 const PostController = require("../controller/posts");
 const router = express.Router();
 const checkAuth = require("../middleware/check-auth");
+const extractFile = require("../middleware/file");
 
-const MIME_TYPE_MAP = {
-    "image/png": "png",
-    "image/jpeg": "jpg",
-    "image/jpg": "jpg"
-};
-const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        const isValid = MIME_TYPE_MAP[file.mimetype];
-        let error = new Error("Invalid mime type");
-        if(isValid) {
-            error = null;
-        }
-        cb(error, "backend/images");
-    },
-    filename: (req, file, cb) => {
-        const name = file.originalname.toLowerCase().split(" ").join("-");
-        const ext = MIME_TYPE_MAP[file.mimetype];
-        cb(null, name + "-" + Date.now() + "." + ext);
-    }
-});
 
-router.post("", checkAuth, multer({storage: storage}).single("image") ,PostController.createPost);
 
-router.get("" ,PostController.getPosts);
+router.post("", checkAuth, extractFile ,PostController.createPost);
+
+router.get("", PostController.getPosts);
 
 router.delete("/:id", checkAuth, PostController.deletePost);
 
-router.put("/:id", checkAuth, multer({storage: storage}).single("image"), PostController.updatePost);
+router.put("/:id", extractFile, PostController.updatePost);
 
-router.get("/:id",PostController.getPost);
+router.get("/:id", PostController.getPost);
 
 module.exports = router;
