c8be418132cdb3566da2444e8c80247e47e229a8
ken0901 <ken5260715@gmail.com>
2024-09-17 22:04:58 -0700

    Modify routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,13 @@
 import { Routes } from '@angular/router';
 import { PostListComponent } from './posts/post-list/post-list.component';
 import { PostCreateComponent } from './posts/post-create/post-create.component';
-import { LoginComponent } from './auth/login/login.component';
-import { SignupComponent } from './auth/signup/signup.component';
 import { AuthGuard } from './auth/auth.guard';
+import { authRoutes } from './route/auth.routes';
 
 export const routes: Routes = [
     { path:'', component:PostListComponent },
     { path:'create', component: PostCreateComponent, canActivate: [AuthGuard]},
     { path:'edit/:postId', component: PostCreateComponent, canActivate: [AuthGuard]},
-    { path: 'login', component: LoginComponent},
-    { path: 'signup', component: SignupComponent}
+    { path: 'auth', loadChildren: () => import('./route/auth.routes').then(m => authRoutes)}
+    
 ];
